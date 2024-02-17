6ad792b63146101a87727243d5fd6912ef76ac1b
Vivo <146621450+vivolev647@users.noreply.github.com>
2024-02-17 17:36:23 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,125 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "derprilla":return img`
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f d d d d f f f f . . 
+. f f f f d d d d d d f f f f . 
+. f f f d d d d d d d d f f f . 
+. f f f d d 6 d d 6 d d f f f . 
+. f f f f 1 1 1 1 1 1 f f f f . 
+. f f f f 1 b 1 1 b 1 f f f f . 
+. f f f f 1 1 b b 1 1 f f f f . 
+. f f f f f 1 1 1 1 f f f f f . 
+. f f f f f 1 1 1 1 f f f f f . 
+. f f f f f f 1 1 f f f f f f . 
+. f f f f f f 1 1 f f f f f f . 
+. . f f f f f f f f f f f f . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "you-world":return img`
+. . . . f f f f . . . . . 
+. . f f f f f f f f . . . 
+. f f f f f f c f f f . . 
+f f f f f f c c f f f c . 
+f f f c f f f f f f f c . 
+c c c f f f b b f f c c . 
+f f f f f b b f f c c f . 
+f f f b f b b f b f f f . 
+. f d 1 f d d f 1 d f . . 
+. f b d d d d d d b f . . 
+. f f f b b b b f f f . . 
+f b f 6 7 7 7 7 6 f b f . 
+b d f 7 7 7 7 7 7 f d b . 
+b b f 6 6 6 6 6 6 f b b . 
+. . . f f f f f f . . . . 
+. . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "derpachu":return img`
+bbbb........bbbb.................
+cddbb......bbddb.................
+cdddbb....bbdddc.................
+cdbddbccccbddbdc.................
+cdbbddbccbddbbdc.................
+cd3bddddddddb3dc.................
+cd33dddddddd33dc.................
+cdd3dddddddd3ddc.................
+cddddddd11dddddc.................
+cdddffd111dffddc........bbbbbb...
+cdddffd1111ffddc.......cddddddbb.
+cdddd1111111dddc......cddddddddb.
+cddd1111fff11ddc.....cddd111ddddb
+cdddc11fff11ddbc.....cdd11111dddb
+cdddcc111111cdbc.....cd11dd111ddb
+cdddddbb33ccddbcc....cbddbbd11ddc
+cddddddb33cdddddbbccccbbdbbb11ddc
+cdddddddbbddddddddddddddddbb1dddc
+cdddd1111dddddddddddddddddddbdddc
+cddd111111dddddddddddddddddddddc.
+cddd1111111dddddddddddddddddddcc.
+cdd11111111dddddddddddddddddddc..
+cdd111111111ddddddddddddddddddc..
+cbd111111111ddddddddddddddddddc..
+.fd111111111ddddddddddddddddddc..
+.ff111111111ddddddddddddddddddc..
+..fb11111111ddddddddddddddddddc..
+...fb111111ddddddd111111ddddddc..
+...fbbb1111dddddd11111111dddddc..
+....fbbfffbddddccccccccccbddddc..
+....fbbf..fdddc.....fbbf.cddddc..
+....fbbf..fdddc.....fbbf.ccddddc.
+....fbbf..fddc.......fbf..ccdddc.
+....fbbf..fddc.......fbbf..ccddc.
+....fbbf..fddc.......fbbf...cddc.
+....fbbf..fddc......fbbbf...cddc.
+...fbbbf..fddc......ffff....cbdc.
+...fbbf..fdddc.............cdddc.
+...ffff..fddcc.............cdddc.
+.........fffc..............cccc..
+`;
+            case "image3":
+            case "african slave":return img`
+. . . . f f f f f f f f . . . . 
+. . . f f e e e e e e f f . . . 
+. . f f e e e e e e e e f f . . 
+. . f e e e e e e e e e e f . . 
+. f f e f f f e e f f f e f f . 
+. f e e 1 1 1 e e 1 1 1 e e f . 
+. f e e 1 f 1 e e 1 f 1 e e f . 
+. f f e 1 1 1 e e 1 1 1 e f f . 
+. . f e e e e e e e e e e f . . 
+. . f e e e e e e e e e e f . . 
+. . f e e e e f f e e e e f . . 
+. . f e e e f 1 1 f e e e f . . 
+. . f e e f f f f f f e e f . . 
+. . . f e e e e e e e e f . . . 
+. . . . f e e e e e e f . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
